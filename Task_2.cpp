ca492560db0019541f1ab0bc774adc8b5b6ac506
XmtosX <63305576+XmtosX@users.noreply.github.com>
2020-08-11 00:03:54 +0300

    Add files via upload

--- Task_2.cpp
+++ Task_2.cpp
@@ -1,24 +1,49 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-float len (float a,float b)
+const float inf= 2e6;
+// a function to calculate the slope of a line
+int up,down;
+float slope (float a, float b)
 {
-    return (sqrt(a*a+b*b));
+    if (a==0)
+        return inf;
+    return (b/a);
+}
+// a function to see if the point (X,Y) is to the up or down side of line y=mx+p
+bool calc (float m, float p,float X,float Y)
+{
+    float a= (m*X+p);
+    if (Y>a)
+        return true;
+    else
+        return false;
 }
 int main()
 {
-    float x,y,xx,yy,xxx,yyy;
-    cout <<"Enter The Coordinations of The Triangle.\n";
-    cin >>x>>y>>xx>>yy>>xxx>>yyy;
-    float len1= len(xx-x,yy-y);
-    float len2= len(xxx-x,yyy-y);
-    float len3= len(xxx-xx,yyy-yy);
-    //Heron's formula
-    float a= (len1+len2+len3)/2.0;
-    float b= (a*(a-len1)*(a-len2)*(a-len3));
-    if (b<=0)
-        cout <<"Invalid Triangle!";
+    float x,y,xx,yy,xxx,yyy,xxxx,yyyy,X,Y;
+    cout <<"Enter The Parallelogram's Coordinations (Clockwise or Counterclockwise) Followed By The Point's Coordinations.\n";
+    cin >>x>>y>>xx>>yy>>xxx>>yyy>>xxxx>>yyyy>>X>>Y;
+    float m1= slope(xx-x,yy-y);
+    float m2= slope(xxx-xx,yyy-yy);
+    float m3= slope(xxxx-xxx,yyyy-yyy);
+    float m4= slope(x-xxxx,y-yyyy);
+    float p1= (y-m1*x);
+    float p2= (yy-m2*xx);
+    float p3= (yyy-m3*xxx);
+    float p4= (yyyy-m4*xxxx);
+    bool yes=false;
+    //seeing if the opposite sides of the parallelogram are on the opposite side of the point
+    if (calc(m1,p1,X,Y)!=calc(m3,p3,X,Y))
+    {
+        if (calc(m2,p2,X,Y)!=calc(m4,p4,X,Y))
+        {
+            yes=true;
+        }
+    }
+    if (yes)
+        cout <<"The Point is Inside The Parallelogram.";
     else
-        cout <<"The Area of The Triangle is: "<<sqrt(b);
+        cout <<"The Point is Outside The Parallelogram.";
     return 0;
 }
